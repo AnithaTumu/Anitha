463ccaa1058576b7403c72d46005394c86da0b46
Ubuntu <ubuntu@ip-172-31-0-49.us-east-2.compute.internal>
2019-09-17 06:40:42 +0000

    Change

--- helloword.java
+++ helloword.java
@@ -4,7 +4,7 @@ class helloword {
 	 
 	    /**
 	     *      * @param args
-	     *           */
+	     */ 
 	    public static void main(String[] args) {
 		            System.out.println("Hello World");
 			        }
